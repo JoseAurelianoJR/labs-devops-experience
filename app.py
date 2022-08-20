609b49cc0e45950b2883d7c2d60a0d79680d5d24
Jose Aureliano <joseaurelianojr@gmail.com>
2022-08-20 11:13:54 -0300

    CSRF Protection

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
+csrf = CSRFProtect(app) 
+
 @app.route("/")
 def pagina_inicial():
-    return "As aulas da gaby são d+!!!"
+    return "DevOps Impacta"
 
 if __name__ == '__main__':
     app.run()
